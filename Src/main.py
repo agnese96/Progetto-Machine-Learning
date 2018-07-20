3151a03d84e6826dfe15a557cfa6d133e778d05e
Agnese Folli <agnesefolli@gmail.com>
2018-07-20 17:13:15 +0200

    Added save and plot

--- Src/main.py
+++ Src/main.py
@@ -5,10 +5,10 @@ path = os.getcwd()
 path
 
 #%% serve per jupyter
-import os
+""" import os
 os.chdir('./Src')
 path='..'
-
+ """
 #%% main imports
 import torch
 from torch.utils.data import DataLoader
@@ -40,12 +40,34 @@ inception = models.inception_v3(pretrained=True)
 
 from copy import deepcopy
 model = deepcopy(inception)
+#print(model)
+
+#%%
+from torch import nn
+model.fc = nn.Linear(2048,16)
+print(model)
 
 #%%
 torch.cuda.empty_cache()
 
 #%%
+modelPath="C:/Users/beaut/Google Drive/Trio++/3°ANNO/Machine Learning/Progetto/Models/"
+#model.load_state_dict(torch.load(modelPath+'ResNet18CrossEntropyReg5_1532030250.256796.pth'))
+
+#%%
 from trainFunction import trainClassification
-modelTrained, classificationLogs = trainClassification(model, imageLoaderTrain, imageLoaderValidation)
+epoch = 1
+modelTrained, classificationLogs = trainClassification(model, imageLoaderTrain, imageLoaderValidation, epochs=epoch)
 
 print(classificationLogs)
+
+#%% save model
+import time
+modelName="InceptionV3CrossEntropyReg%d_%f.pth" % (epoch, time.time())
+torch.save(modelTrained.state_dict(), modelPath+modelName)
+
+#%% 
+from helperFunctions import plot_logs_classification
+
+#%%
+plot_logs_classification(classificationLogs)
