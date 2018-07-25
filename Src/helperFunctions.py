cac2e548c42384ab553ff308c3a4b2e7e90d6215
Agnese Folli <agnesefolli@gmail.com>
2018-07-25 20:07:21 +0200

    REC Da verificare

--- Src/helperFunctions.py
+++ Src/helperFunctions.py
@@ -59,3 +59,30 @@ def plot_logs_regression(logs):
     plt.grid()
     plt.show()
 
+#MODO 2) CURVA ROC 
+#Calcoliamo tutti gli errori tra il valore F(xi) e yi quello predetto. Poi li raggruppiamo in 'bin' e li cumuliamo rappresentandoli via via. 
+def rec_curve(predictions, reals):
+    #calcoliamo gli errori ovvero Ei =(f(xi) - yi)^2
+    errors = (predictions - reals)**2
+    #ogni "errore " sarà un bin. Ovviamente dobbiamo evitare bin duplicaati: . Dopodichè ordiniamo dal più piccolo errore al più grande.
+    #Ci aspettiamo che la maggior degli errori cada nei primi bin, cioè quelli più piccoli. Ogni bin sarà un valore 'x' nell'asse x. 
+    bins =np.sort( np.unique(errors) )
+    #troviamo le frequenze 'cumulate' rispetto ad ogni bin. Ogni elemento sarà un punto 'y' rispetto ad un bin 'x'. 
+    freq=[]
+    for bin in bins: 
+        numElement = (errors <=bin).mean() #trova gli elementi <= all'errore rappresentato dal bin e fanne la media. Usiamo 'mean' perchè 
+        #vogliamo trovare la frazione rispetto al totale elementi( con sum trovavamo il numero di quelli solo <=bin)
+        freq.append( numElement)
+
+    AUC = np.trapz(freq,bins) #calcoliamo l'area sotto la curva ottenuta considerando i bin sull'asse x e le frequenze 'freq' sull'asse y
+    #l'area totale è un rettangolo la cui base è pari al bin più grande sull'asse x mentre l'altezza è pari ad 1, perchè stiamo considerando 
+    #solo frazioni sull'asse y quindi valori compresi tra 0 e 1 ( max. ). Area rettangolo è base per altezza-> lastBin*1
+    #Quindi l'area totale meno quella sotto la curva, ci da quella sopra la curva che deve essere più piccola possibile 
+    totArea = np.max(bins)*1  #area totale
+    AOC = totArea - AUC #area sopra la curva . Deve esserre più piccola possibile
+    #---------vog
+    return bins,freq,AOC #restituisce tutto come una tupla. quindi un array sostanzialmente  
+    
+#boston_linear_regressor_rec =rec_curve(boston_linear_regressor_predictions,boston_linear_regressor_reals)
+
+
