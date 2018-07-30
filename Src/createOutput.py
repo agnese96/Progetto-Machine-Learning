e13554b7eeeb8d3067ec1ec69ee8724150837134
Agnese Folli <agnesefolli@gmail.com>
2018-07-30 20:38:27 +0200

    Started working on createOutput

--- Src/createOutput.py
+++ Src/createOutput.py
@@ -17,6 +17,7 @@ transform = transforms.Compose([transforms.Resize([224,224]),
                                 transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                      std=[0.229, 0.224, 0.225])])
+                                                     
 classificationDataset = ImageDataset(path+'/Dataset/images', path+'/Dataset/testing_list_blind.csv', transform=transform)
 
 #%%
@@ -25,7 +26,5 @@ modelPath="C:/Users/beaut/Google Drive/Trio++/3°ANNO/Machine Learning/Progetto/
 modelName = modelPath+'ResNet18CrossEntropyReg10_1532170755.190167.pth'
 classificationModel = getClassificationModel(previous_state_path=modelName)
 
-#%% 
-#%% 
-CNNOutputTrain = torch.load(modelPath+'FeaturesResNet18Train512.pth')
-CNNOutputValidation = torch.load(modelPath+'FeaturesResNet18Validation512.pth')
+#%% TODO: add load test features regression after creating them with extractFeatures
+ 
